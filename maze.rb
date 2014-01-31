337337665f02291f165d5c62da5d5b478e6de8bc
Chi Ieong (Eddie) Tai <chiieong@brandeis.edu>
2014-01-31 01:47:53 -0500

    Partially finished methods

--- maze.rb
+++ maze.rb
@@ -4,20 +4,37 @@ class Maze
  
   def initialize(n,m)
     
-    @maze_as_string = Array.new
-    
+    @maze_as_string = Array.new (n) { Array.new (m) }
+    @n = n
+    @m = m
     
   end
  
   def load (arg)
     
-    @maze_as_string = arg.split("")
+    @maze_as_string = arg.split("").each_slice(@n).to_a
+     
+  end
+  
+  def display
+    
+    @maze_as_string.each do |key|
+      
+      key.each do |key2|
+      print key2
+      end
+      
+      puts 
+      
+    end
     
     
   end
+  
 end
 
 
-test = Maze.new(2,3)
+test = Maze.new(9,9)
 test.load("111111111100010001111010101100010101101110101100000101111011101100000101111111111")
-p test.maze_as_string
+#p test.maze_as_string
+test.display
