e82d167807522d1240d718b3ead5b1144ac4a254
Eddie Tai <teddy123168@gmail.com>
2014-02-10 23:25:00 -0500

    Final commit

--- maze.rb
+++ maze.rb
@@ -1,10 +1,16 @@
-require_relative 'node.rb'
-require_relative 'point.rb'
+#Chi Ieong (Eddie) Tai
+#COSI 236b PA3
+#maze.rb allows one to create a maze of zeros and ones
+#and find a possible solution to that maze.
+
 
 class Maze
  
  attr_accessor :maze_as_string
  
+  #initialize maze,size of maze n by m, a node, and a point representing 
+  #a coordinate within the maze
+  #returns void
   def initialize(n,m)
     
     @maze = Array.new (n) { Array.new (m) }
@@ -12,41 +18,41 @@ class Maze
     @m = m
     @node = Struct.new(:parent, :value)
     @point = Struct.new(:x, :y)
-    
+   
   end
  
+  #loads maze using the provided string argument
+  #returns void
   def load (arg)
     
     @maze = arg.split("").each_slice(@n).to_a 
-    p @maze
-     
+
   end
-  
-  def display
     
+  #displays the maze
+  #returns void
+  def display
     @maze.each do |key|
-      
       key.each do |key2|
       print key2
       end
-      
       puts 
-      
-    end
-    
+    end 
   end
  
+  #determines whether a maze is solvable or not.
+  #returns true if solvable or string "Error" if it is not.
   def solve(begX,begY,endX,endY)
     queue = Queue.new
     initial_node = @point.new(begX,begY)
     final_node = @point.new(endX,endY)
-    queue.push(initial_node)  
+    queue.push(initial_node) #push initial node to queue and then perform BFS.
     while (not queue.empty?) do
     value = queue.deq
-      if value == final_node 
-        return value
+      if value == final_node #the node we want
+        return true
       else
-      solutions = get_solutions(value)
+      solutions = get_solutions(value) #get array of solutions
         solutions.each do |value2|
         queue.push(value2)
         end
@@ -55,28 +61,19 @@ class Maze
     return "Error"
   end
   
+  #traces the path of the solution to the maze using BFS.
+  #similar to the method solve except a node in this method
+  #keeps track of its parent.
+  #returns void
   def trace(begX,begY,endX,endY)
     queue = Queue.new
-    path = Array.new
-    initial_point = @point.new(begX,begY)
     final_point = @point.new(endX,endY)
-    initial_node = @node.new(@node.new(nil,@point.new(-1,-1)),initial_point)
+    initial_node = @node.new(@node.new(nil,@point.new(-1,-1)),@point.new(begX,begY))
     queue.push(initial_node)
     while (not queue.empty?) do
     value = queue.deq
       if value.value == final_point 
-          track = Array.new
-          track << value.value        
-          a = value
-          while(a.parent.value.x != -1 && a.parent.value.y != -1) do
-            track << a.parent.value
-            a = a.parent
-          end
-          track.reverse.each do |value2| 
-            print value2.x
-            print " "
-            p value2.y
-          end
+          print_tree(value)
           return
        else
        solutions = get_solutions(value)
@@ -88,12 +85,31 @@ class Maze
     p "Error"
   end
    
-  
+  #prints node tree in reverse order or from the beginning node
+  #to the end node
+  def print_tree(value)
+    track = Array.new
+          track << value.value        
+          a = value
+          while(a.parent.value.x != -1 && a.parent.value.y != -1) do
+            track << a.parent.value
+            a = a.parent
+          end
+          track.reverse.each do |value2| 
+            print value2.x
+            print " "
+            p value2.y
+          end
+  end 
+   
+   
+  #gets the solution of a maze coordinate given by a node
+  #returns an array of solutions to the maze coordinate.
   def get_solutions(node)
     solutions = Array.new
     x = node.value.x
     y = node.value.y    
-    #left
+    #check left of coordinate
     if (x-1 >= 0) && ((x-1) != node.parent.value.x || y != node.parent.value.y) && (@maze[y][x-1] == "0") 
       solutions << @node.new(node,@point.new(x-1,y))
     end
@@ -111,11 +127,6 @@ class Maze
     end
     return solutions    
   end
+  
 end
-
-  test = Maze.new(9,9)
-  test.load("111111111100010001111010101100010101101110101100000101111011101100000101111111111")
-  #p test.maze_as_string
-  test.display
-  test.trace(1,1,5,7)
   
---
+++ maze_generator.rb
@@ -0,0 +1,122 @@
+class MazeGenerator
+  
+  
+  def initialize(n,m)
+    
+    @maze = Array.new (n) { Array.new (m) }
+    arg = "1" * (n*m)
+    @maze = arg.split("").each_slice(n).to_a 
+    @n = n
+    @m = m
+    @maze_track = Array.new (n) { Array.new (m) }
+    @point = Struct.new(:x, :y, :previousPoint)
+    
+    #@maze[2][0] = 1
+    
+    @maze.each_with_index do |y, index|
+      
+    
+        y.each_with_index do |x, index2|
+        
+        
+        if(index == 0 || index == (m-1) || index2 == 0 || index2 == (n-1))
+        @maze[index][index2] = 2
+        
+        end
+        
+      end
+      
+    end 
+    
+    #p @maze
+  end
+  
+  def display
+    
+    @maze.each do |key|
+      
+      key.each do |key2|
+      print key2
+      end
+      
+      puts 
+      
+    end
+    
+  end
+ 
+
+  def generate
+    
+    x = 1
+    
+    prng = Random.new
+    start_x = prng.rand(1..(@n-2))
+    start_y = prng.rand(1..(@m-2))
+    
+    starting_point = @point.new(-1,-1,@point.new(-1,-1))
+    finished_point = @point.new(start_x,start_y,@point.new(0,0))
+    
+    while ((starting_point.x != finished_point.x) || (starting_point.y != finished_point.y)) do
+
+      if(x==1)
+        starting_point = finished_point
+        @maze[starting_point.y][starting_point.x] = 0
+        @maze_track[starting_point.y][starting_point.x] = 1
+        x = x + 1
+      end
+      
+      point = get_solutions(starting_point)
+   
+   
+      if(point != 0 && (@maze_track[point.y][point.x] !=1))
+         
+        @maze[point.y][point.x] = 0
+        @maze_track[point.y][point.x] = 1
+        
+        starting_point = @point.new(point.x,point.y,starting_point)
+        
+      else
+        starting_point = starting_point.previousPoint
+
+      end
+      
+      display()
+      puts 
+            
+    end
+    
+  end
+  
+  def get_solutions(point)
+
+    x = point.x
+    y = point.y
+    
+    #left
+    if(@maze[y][x-1] == "1")
+      return @point.new(x-1,y)
+    end
+    #right
+    if(@maze[y][x+1] == "1")
+      return @point.new(x+1,y)
+    end
+    #top
+    if(@maze[y+1][x] == "1")
+      return @point.new(x,y+1)
+    end
+    #down
+    if(@maze[y-1][x] == "1")
+      return @point.new(x,y-1)
+    end
+    
+    return 0
+  end
+  
+end
+
+test = MazeGenerator.new(9,9)
+
+test.display()
+
+test.generate()
